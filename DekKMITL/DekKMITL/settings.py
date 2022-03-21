06353239a45dea773cfac419ab2be761352ae8ab
Sirinda Wangfaen <74582141+SirindaW@users.noreply.github.com>
2022-03-22 00:12:48 +0700

    add - further work on blog and tag

--- DekKMITL/DekKMITL/settings.py
+++ DekKMITL/DekKMITL/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
+    'blog',
+    'taggit',
 ]
+
+TAGGIT_CASE_INSENSITIVE = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- DekKMITL/blog/models.py
+++ DekKMITL/blog/models.py
@@ -1,3 +1,22 @@
+from time import timezone
 from django.db import models
+from taggit.managers import TaggableManager
 
 # Create your models here.
+class Category(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=200,unique=True,default='')
+    
+    def __str__(self): 
+        return self.title
+    
+    
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True,blank=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    post_likes = models.PositiveIntegerField(default=0,null=False)
+    post_fav = models.PositiveBigIntegerField(default=0,null=False)
+    slug = models.SlugField(unique=True,max_length=300)
+    tags = TaggableManager()
