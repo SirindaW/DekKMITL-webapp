36816a42a8dd965bad10c2738a9ed75ae59f8f8c
Sirinda Wangfaen <74582141+SirindaW@users.noreply.github.com>
2022-04-17 09:02:41 +0700

    view large profile

--- DekKMITL/statics/js/main.js
+++ DekKMITL/statics/js/main.js
@@ -12,9 +12,28 @@ function showTime() {
     document.getElementById("time_choose_dropdown").classList.toggle("time-show");
 }
 
+// show full cover image
+function fullCoverImg() {
+    document.getElementById("full_cover_img").classList.add("img-show");
+}
 
-// Close the dropdown if the user clicks outside of it
+// close full cover image
+function closeFullCoverImg() {
+    document.getElementById("full_cover_img").classList.remove("img-show");
+}
+
+// show full profile image
+function fullProfileImg() {
+    document.getElementById("full_profile_img").classList.add("img-show");
+}
+
+// close full profile image
+function closeFullProfileImg() {
+    document.getElementById("full_profile_img").classList.remove("img-show");
+}
+
 window.onclick = function(event) {
+    // Close the dropdown if the user clicks outside of it
     if (!event.target.matches('.user-profile-navbar')) {
         var dropdowns = document.getElementsByClassName("profile-dropdown-content");
         var i;
@@ -25,6 +44,8 @@ window.onclick = function(event) {
             }
         }
     }
+
+    // Close the dropdown if the user clicks outside of it
     if (!event.target.matches('.time-choose-dropbtn')) {
         var dropdowns = document.getElementsByClassName("time-choose-dropdown-content");
         var i;
@@ -35,13 +56,39 @@ window.onclick = function(event) {
             }
         }
     }
+
+    // Close toolbar if the user clicks outside of it
     if (!event.target.matches('.hide-toolbar')) {
-        var dropdowns = document.getElementsByClassName("toolbar-container");
+        var toolbar = document.getElementsByClassName("toolbar-container");
         var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('toolbar-show')) {
-                openDropdown.classList.remove('toolbar-show');
+        for (i = 0; i < toolbar.length; i++) {
+            var temp = toolbar[i];
+            if (temp.classList.contains('toolbar-show')) {
+                temp.classList.remove('toolbar-show');
+            }
+        }
+    }
+
+    // Close profile cover if the user clicks outside of it
+    if (!event.target.matches('.profile-cover')) {
+        var img = document.getElementsByClassName("popup_cover_img");
+        var i;
+        for (i = 0; i < img.length; i++) {
+            var temp = img[i];
+            if (temp.classList.contains('img-show')) {
+                temp.classList.remove('img-show');
+            }
+        }
+    }
+
+    // Close profile picture if the user clicks outside of it
+    if (!event.target.matches('.profile-picture')) {
+        var img = document.getElementsByClassName("popup_profile_img");
+        var i;
+        for (i = 0; i < img.length; i++) {
+            var temp = img[i];
+            if (temp.classList.contains('img-show')) {
+                temp.classList.remove('img-show');
             }
         }
     }
