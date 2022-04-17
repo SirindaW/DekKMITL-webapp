6f17325034f2ff153995839eeedc578d472b8f12
Sirinda Wangfaen <74582141+SirindaW@users.noreply.github.com>
2022-04-17 07:10:40 +0700

    home html edited
    แก้ effect เลือกช่วงเวลา top3

--- DekKMITL/statics/js/main.js
+++ DekKMITL/statics/js/main.js
@@ -30,8 +30,8 @@ window.onclick = function(event) {
         var i;
         for (i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
+            if (openDropdown.classList.contains('time-show')) {
+                openDropdown.classList.remove('time-show');
             }
         }
     }
