94aeff805dcd248c39fe7f57cd922b7725171089
Sirinda Wangfaen <74582141+SirindaW@users.noreply.github.com>
2022-04-22 04:54:16 +0700

    search follower following

---
+++ DekKMITL/statics/js/base.js
@@ -0,0 +1,128 @@
+// prevent form submit by enter key
+jQuery(function($) {
+    $('form').on('keydown', function(ev) {
+        if (ev.key === "Enter" && !$(ev.target).is('textarea')) {
+            ev.preventDefault();
+        }
+    });
+
+});
+
+// show dropdown profile
+function showDropdown() {
+    document.getElementById("profile_dropdown").classList.toggle("profile-show");
+}
+
+// show toolbar
+function showToolbar() {
+    document.getElementById("toolbar_container").classList.toggle("toolbar-show");
+}
+
+function showTime() {
+    document.getElementById("time_choose_dropdown").classList.toggle("time-show");
+}
+
+// show full cover image
+function fullCoverImg() {
+    document.getElementById("full_cover_img").classList.add("img-show");
+}
+
+// close full cover image
+function closeFullCoverImg() {
+    document.getElementById("full_cover_img").classList.remove("img-show");
+}
+
+// show full profile image
+function fullProfileImg() {
+    document.getElementById("full_profile_img").classList.add("img-show");
+}
+
+// close full profile image
+function closeFullProfileImg() {
+    document.getElementById("full_profile_img").classList.remove("img-show");
+}
+
+//change unfollow button html text
+function changeText(text) {
+    var display = document.getElementById('unfollow_btn');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+
+function changeBack(text) {
+    var display = document.getElementById('unfollow_btn');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+
+window.onclick = function(event) {
+    // Close the dropdown if the user clicks outside of it
+    if (!event.target.matches('.user-profile-navbar')) {
+        var dropdowns = document.getElementsByClassName("profile-dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('profile-show')) {
+                openDropdown.classList.remove('profile-show');
+            }
+        }
+    }
+
+    // Close the dropdown if the user clicks outside of it
+    if (!event.target.matches('.time-choose-dropbtn')) {
+        var dropdowns = document.getElementsByClassName("time-choose-dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('time-show')) {
+                openDropdown.classList.remove('time-show');
+            }
+        }
+    }
+
+    // Close toolbar if the user clicks outside of it
+    if (!event.target.matches('.hide-toolbar')) {
+        var toolbar = document.getElementsByClassName("toolbar-container");
+        var i;
+        for (i = 0; i < toolbar.length; i++) {
+            var temp = toolbar[i];
+            if (temp.classList.contains('toolbar-show')) {
+                temp.classList.remove('toolbar-show');
+            }
+        }
+    }
+
+    // Close profile cover if the user clicks outside of it
+    if (!event.target.matches('.profile-cover')) {
+        var img = document.getElementsByClassName("popup_cover_img");
+        var i;
+        for (i = 0; i < img.length; i++) {
+            var temp = img[i];
+            if (temp.classList.contains('img-show')) {
+                temp.classList.remove('img-show');
+            }
+        }
+    }
+
+    // Close profile picture if the user clicks outside of it
+    if (!event.target.matches('.profile-picture')) {
+        var img = document.getElementsByClassName("popup_profile_img");
+        var i;
+        for (i = 0; i < img.length; i++) {
+            var temp = img[i];
+            if (temp.classList.contains('img-show')) {
+                temp.classList.remove('img-show');
+            }
+        }
+    }
+}
+
+// search script
+// var input = document.getElementById("mysearch");
+//     input.addEventListener("keyup", function(event) {
+//         if (event.keyCode === 13) {
+//             event.preventDefault();
+//             var q = input.value
+//             location.replace(q)
+//         }
+//     });
--- DekKMITL/statics/js/main.js
+++ DekKMITL/statics/js/main.js
@@ -1,129 +1,35 @@
-// prevent form submit by enter key
-jQuery(function($) {
-    $('form').on('keydown', function(ev) {
-        if (ev.key === "Enter" && !$(ev.target).is('textarea')) {
-            ev.preventDefault();
-        }
-    });
-
-});
-
-// show dropdown profile
-function showDropdown() {
-    document.getElementById("profile_dropdown").classList.toggle("profile-show");
-}
-
-// show toolbar
-function showToolbar() {
-    document.getElementById("toolbar_container").classList.toggle("toolbar-show");
-}
-
-function showTime() {
-    document.getElementById("time_choose_dropdown").classList.toggle("time-show");
-}
-
-// show full cover image
-function fullCoverImg() {
-    document.getElementById("full_cover_img").classList.add("img-show");
-}
-
-// close full cover image
-function closeFullCoverImg() {
-    document.getElementById("full_cover_img").classList.remove("img-show");
-}
-
-// show full profile image
-function fullProfileImg() {
-    document.getElementById("full_profile_img").classList.add("img-show");
-}
-
-// close full profile image
-function closeFullProfileImg() {
-    document.getElementById("full_profile_img").classList.remove("img-show");
-}
-
-//change unfollow button html text
-function changeText(text) {
-    var display = document.getElementById('unfollow_btn');
-    display.innerHTML = "";
-    display.innerHTML = text;
-}
-
-function changeBack(text) {
-    var display = document.getElementById('unfollow_btn');
-    display.innerHTML = "";
-    display.innerHTML = text;
-}
-
-
-window.onclick = function(event) {
-    // Close the dropdown if the user clicks outside of it
-    if (!event.target.matches('.user-profile-navbar')) {
-        var dropdowns = document.getElementsByClassName("profile-dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('profile-show')) {
-                openDropdown.classList.remove('profile-show');
-            }
-        }
-    }
-
-    // Close the dropdown if the user clicks outside of it
-    if (!event.target.matches('.time-choose-dropbtn')) {
-        var dropdowns = document.getElementsByClassName("time-choose-dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('time-show')) {
-                openDropdown.classList.remove('time-show');
-            }
-        }
-    }
-
-    // Close toolbar if the user clicks outside of it
-    if (!event.target.matches('.hide-toolbar')) {
-        var toolbar = document.getElementsByClassName("toolbar-container");
-        var i;
-        for (i = 0; i < toolbar.length; i++) {
-            var temp = toolbar[i];
-            if (temp.classList.contains('toolbar-show')) {
-                temp.classList.remove('toolbar-show');
-            }
-        }
-    }
-
-    // Close profile cover if the user clicks outside of it
-    if (!event.target.matches('.profile-cover')) {
-        var img = document.getElementsByClassName("popup_cover_img");
-        var i;
-        for (i = 0; i < img.length; i++) {
-            var temp = img[i];
-            if (temp.classList.contains('img-show')) {
-                temp.classList.remove('img-show');
-            }
-        }
-    }
-
-    // Close profile picture if the user clicks outside of it
-    if (!event.target.matches('.profile-picture')) {
-        var img = document.getElementsByClassName("popup_profile_img");
-        var i;
-        for (i = 0; i < img.length; i++) {
-            var temp = img[i];
-            if (temp.classList.contains('img-show')) {
-                temp.classList.remove('img-show');
-            }
+//search not found display 
+function displayCheck(id, show) {
+    notfound = document.getElementById(show);
+    table = document.getElementById(id);
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        if (tr[i].style.display == 'none') {
+            notfound.style.display = "flex";
+        } else {
+            notfound.style.display = "none";
         }
     }
 }
 
-// search script
-// var input = document.getElementById("mysearch");
-//     input.addEventListener("keyup", function(event) {
-//         if (event.keyCode === 13) {
-//             event.preventDefault();
-//             var q = input.value
-//             location.replace(q)
-//         }
-//     });
+//search
+function search(notfoundID, inputID, tableID, checkRow) {
+    var input, filter, table, tr, td, i, txtValue;
+    notfound = document.getElementById(notfoundID);
+    input = document.getElementById(inputID);
+    filter = input.value.toUpperCase();
+    table = document.getElementById(tableID);
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[checkRow];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+    displayCheck(tableID, notfoundID);
+}
